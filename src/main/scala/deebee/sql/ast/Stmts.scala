185e18cb98d26d7edc21180a8e4996d8e26c04a2
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-21 14:55:07 -0500

    Implement some of the abstract-syntax tree

---
+++ src/main/scala/deebee/sql/ast/Stmts.scala
@@ -0,0 +1,24 @@
+package deebee.sql.ast
+
+/**
+ * Created by hawk on 11/21/14.
+ */
+sealed trait Stmt extends Node
+case class Select(
+  projections: List[Proj] = GlobProj :: Nil,
+  from: List[Const[String]],
+  where: Option[Predicate] = None,
+  //TODO: implement these
+  //groupBy: Option[GroupBy] = None,
+  //orderBy: Option[OrderBy] = None,
+  limit: Option[Int] = None
+                   ) extends Stmt {
+
+  override def emitSQL =
+    s"SELECT  ${projections.map(_.emitSQL).mkString(", ")}" +
+    s" FROM ${from.map(_.emitSQL).mkString(",")} "+
+    s"${where.map("WHERE" + _.emitSQL)}${limit.map(" LIMIT " + _)};"
+    //groupBy.map(_.emitSQL),
+    //orderBy.map(_.emitSQL),
+
+}

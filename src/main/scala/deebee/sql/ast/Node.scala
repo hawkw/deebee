f45d0dd2d1d4084339759ed403bab915982262b7
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-22 14:12:44 -0500

    Add tabs to schema SQL

--- src/main/scala/deebee/sql/ast/Node.scala
+++ src/main/scala/deebee/sql/ast/Node.scala
@@ -1,10 +1,19 @@
 package deebee.sql.ast
 
 /**
+ * Trait for a node in a SQL abstract syntax tree.
+ *
+ * @author Hawk Weisman
+ *
  * Created by hawk on 11/21/14.
  */
 trait Node {
+  /**
+   * Re-emit the SQL statement(s) corresponding to this node
+   * @return
+   */
   def emitSQL: String = ???
+  override def toString = emitSQL
 }
 sealed trait Expr[T] extends Node
 case class Const[T](x: T) extends Expr[T] {
@@ -25,7 +34,7 @@ case class Schema(
   constraints: List[Constraint] = Nil
   ) extends Node {
   override def emitSQL = {
-    s"CREATE TABLE $name (\n${attributes.map(_.emitSQL).mkString("\n")}\n" +
-      s"${constraints.map(_.emitSQL).mkString("\n")});"
+    s"CREATE TABLE $name (\n\t${attributes.map(_.emitSQL).mkString("\n\t")}\n" +
+      s"${constraints.map(_.emitSQL).mkString("\n\t")});"
   }
 }

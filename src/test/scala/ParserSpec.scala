fed04da12824ae0f7f6b48984667c844968f9bdb
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-22 15:44:22 -0500

    More create tests

--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -9,10 +9,32 @@ import scala.util.{Try, Success}
  * Created by hawk on 11/22/14.
  */
 class ParserSpec extends FlatSpec with Matchers {
-  "The SQL Parser" should "parse a simple CREATE TABLE sequence" in {
-    val create = "CREATE TABLE Test (\ntest INTEGER PRIMARY KEY NOT NULL\n);"
-    val result: Try[Node] = SQLParser.parse(create)
+
+  /**
+   * Helper method for testing. Asserts that the a SQL query can be reconstructed from its'
+   * AST representation (which should more or less ensure that it was parsed correctly).
+   * @param sql the SQL query to test
+   */
+  private def assertReconstructed (sql: String): Unit = {
+    val result: Try[Node] = SQLParser.parse(sql)
     result shouldBe a [Success[Node]]
-    result.get.emitSQL should include (create)
+    result.get.emitSQL should include (sql)
+  }
+  "The SQL Parser" should "parse a simple CREATE TABLE statement" in {
+    assertReconstructed(
+      "CREATE TABLE Test (\n" +
+        "\ttest INTEGER PRIMARY KEY NOT NULL" +
+      "\n);"
+    )
+  }
+
+  it should "parse a CREATE TABLE statement with multiple attributes and types" in {
+    assertReconstructed("CREATE TABLE Test (\n" +
+      "\ttestint INTEGER PRIMARY KEY NOT NULL,\n" +
+      "\ttestvarchar VARCHAR(255),\n" +
+      "\ttestnumeric NUMERIC(5, 8) NOT NULL,\n" +
+      "\ttestchar CHAR(15)\n" +
+      ");"
+    )
   }
 }

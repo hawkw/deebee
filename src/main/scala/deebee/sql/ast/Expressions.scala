62231b348323990281202b840ce27940179cf690
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-23 13:56:55 -0500

    Add Expressions (whoops)

---
+++ src/main/scala/deebee/sql/ast/Expressions.scala
@@ -0,0 +1,20 @@
+package deebee
+package sql.ast
+import scala.reflect.runtime.universe._
+
+/**
+ * Created by hawk on 11/23/14.
+ */
+sealed trait Expr[T] extends Node {
+  val typeof = weakTypeTag[T]
+}
+case class Const[T](x: T) extends Expr[T] {
+  override val emitSQL = x.toString
+}
+case class Ident(name: String) extends Expr[String] {
+  override val emitSQL = name
+}
+case class Comparison(left: Expr[_], op: String, right: Expr[_]) extends Expr[Boolean] {
+  override lazy val emitSQL = s"$left $op $right"
+  def emitPredicate(context: Relation) = ??? // TODO: this would generate the actual predicate to be used by the query executor
+}

767d87075ce4e994b02f1dc4d2cfa94486e90b9b
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-24 17:00:54 -0500

    Add travis script for publishing ScalaDoc

---
+++ .travis/publish.sh
@@ -0,0 +1,21 @@
+if [ "$TRAVIS_REPO_SLUG" == "hawkw/deebee" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && ["$TRAVIS_SCALA_VERSION" == "2.11.4"]; then
+
+	echo -e "Publishing ScalaDoc...\n"
+
+	cp -R target/scala-2.11/api $HOME/api
+
+	cd $HOME
+	git config --global user.email "travis@travis-ci.org"
+	git config --global user.name "travis-ci"
+	git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hawkw/deebee gh-pages > /dev/null
+
+	cd gh-pages
+	git rm -rf ./api
+	cp -Rf $HOME/api .
+	git add -f .
+	git commit -m "Lastest ScalaDoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+	git push -fq origin gh-pages > /dev/null
+
+	echo -e "Published ScalaDoc to gh-pages.\n"
+
+fi

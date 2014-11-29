596fc2f3041408bfc75b2b5998c984241853d0ef
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-29 17:06:23 -0500

    Start on new tests for Relation API

--- src/test/scala/QueryProcessingSpec.scala
+++ src/test/scala/QueryProcessingSpec.scala
@@ -1,30 +1,32 @@
-import deebee.storage.Relation
-import org.scalamock.scalatest.MockFactory
-import org.scalatest.{WordSpec, FunSpec, Matchers, FlatSpec}
+
+import org.scalatest.{GivenWhenThen, Matchers, WordSpec}
 
 /**
  * Created by hawk on 11/27/14.
  */
-class QueryProcessingSpec extends WordSpec with Matchers with MockFactory {
-  "A SELECT statement" when {
-    "in the simplest form (SELECT * FROM table)" should {
-      "produce the correct result" in {
+class QueryProcessingSpec extends WordSpec with Matchers with GivenWhenThen {
+
+  "A Relation" when {
+    "in memory" should {
+      "provide access to its' rows" in {
+        pending
+      }
+      "project a new relation with the given attributes" in {
+        pending
+      }
+      "correctly filter by a given predicate, returning a new relation" in {
         pending
       }
     }
-    "with a list of projections (SELECT one, two FROM table)" should {
-      "produce the correct result" in {
+    "backed by a CSV" should {
+      "provide access to its' rows" in {
         pending
       }
-    }
-    "with a WHERE clause and the glob projection (SELECT * FROM table WHERE cond)" should {
-        "produce the correct result" in {
-          pending
-        }
-      "with a WHERE clause and a list of projections (SELECT one, two FROM table WHERE cond)" should {
-        "produce the correct result" in {
-          pending
-        }
+      "project a new relation with the given attributes" in {
+        pending
+      }
+      "correctly filter by a given predicate, returning a new relation" in {
+        pending
       }
     }
   }

cea9af5dd13ce284b5e53f673c7905c0a954d898
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-02 15:41:53 -0500

    Add LIMIT BY clause to SELECT integration spec

--- src/test/scala/IntegrationSpec.scala
+++ src/test/scala/IntegrationSpec.scala
@@ -132,6 +132,39 @@ class IntegrationSpec extends FeatureSpec with Matchers with GivenWhenThen {
       And("the results should only contain columns matching the predicate")
       result.get.toString should equal("|Gregory|Kapfhammer")
     }
+    scenario("an in-memory relation receives a `SELECT` statement with a `LIMIT` clause") {
+
+      Given("a simple in-memory relation")
+      val faculty = new View(
+        Set[Row](
+          Seq[Entry[_]](new IntegerEntry(1), new VarcharEntry("Gregory", 25), new VarcharEntry("Kapfhammer", 25), new VarcharEntry("Alden 106", 25)),
+          Seq[Entry[_]](new IntegerEntry(2), new VarcharEntry("Robert", 25), new VarcharEntry("Roos", 25), new VarcharEntry("Alden 107", 25)),
+          Seq[Entry[_]](new IntegerEntry(3), new VarcharEntry("Janyl", 25), new VarcharEntry("Jumadinova", 25), new VarcharEntry("Alden 107", 25)),
+          Seq[Entry[_]](new IntegerEntry(4), new VarcharEntry("John", 25), new VarcharEntry("Wenskovitch", 25), new VarcharEntry("Alden 108", 25))
+        ),
+        Seq[Attribute[_]](
+          Attribute("id", IntegerType, List(Primary_Key, Not_Null)),
+          Attribute("first_name", VarcharType(25), Nil),
+          Attribute("last_name", VarcharType(25), Nil),
+          Attribute("office", VarcharType(25), Nil)
+        )
+      ) with Selectable
+      When("the relation is queried")
+      val query = SQLParser.parse("SELECT * FROM faculty LIMIT 1;").get
+
+      Then("the parser should parse the query as a SELECT statement")
+      query shouldBe a [SelectStmt]
+
+      And("the query should be processed successfully")
+      val result = faculty.process(query.asInstanceOf[SelectStmt])
+      result shouldBe a [Success[_]]
+
+      And("the result set should contain the requested number of rows")
+      result.get.rows should have size 1
+      And("the result set should match the query")
+      result.get.toString should equal ("|1|Gregory|Kapfhammer|Alden 106")
+
+    }
   }
   feature("DELETE statements are processed correctly.") {
     pending

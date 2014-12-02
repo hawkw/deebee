e955b36acc98ed226af14513b7f3121a677b086f
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-02 13:26:30 -0500

    Add documentation to Relation API

--- src/main/scala/deebee/Relation.scala
+++ src/main/scala/deebee/Relation.scala
@@ -1,11 +1,15 @@
 package deebee
 
 import deebee.exceptions.QueryException
-import deebee.sql.ast.{SelectStmt, Proj, GlobProj, Attribute}
+import deebee.sql.ast._
 
 import scala.util.{Try, Failure, Success}
 
 /**
+ * Common operations for all Relations.
+ *
+ * @author Hawk Weisman <hawk@meteorcodelabs.com>
+ *
  * Created by hawk on 11/29/14.
  */
 trait Relation {
@@ -15,7 +19,26 @@ trait Relation {
    * @return the whole table, as a set of lists of entries
    */
   def rows: Set[Row]
+
   def attributes: Seq[Attribute[_]]
+
+  /**
+   * Add a new [[Row]] to this [[Relation]], returning a [[Try]] on a [[Relation]]with the row appended.
+   *
+   * In the case of mutable [[Relation]]s (i.e. [[deebee.storage.CSVRelation CSVRelation]]), this
+   * can be a reference back to the same relation after it has been modified; while in the course of
+   * immutable relations (i.e. [[View]]) this can be a new relation with those rows appended. This
+   * is intended to ensure that the code for processing `INSERT` statements is polymorphic
+   * across mutable and immutable [[Relation]]s.
+   *
+   * If the relation cannot be modified, this returns a [[Try]] containing an
+   * [[deebee.exceptions.InternalStateException]].
+   *
+   * @param row the row to add
+   * @return a [[Try]] on a reference to a [[Relation]] with the row appended
+   */
+  protected def add(row: Row): Try[Relation]
+
   def project(names: Seq[String]): Relation = new View(
     rows.map(
       r => names.map(
@@ -41,7 +64,8 @@ trait Relation {
 }
 
 trait Selectable extends Relation {
-  def processSelect(select: SelectStmt): Try[Relation] = {
+
+  def process(select: SelectStmt): Try[Relation] = {
     val predicate = select.where
       .map(clause => clause.emit(this))
     (select.projections match {
@@ -63,9 +87,39 @@ trait Selectable extends Relation {
       )
       )
   }
+
+}
+trait Modifyable extends Relation {
+  def process(insert: InsertStmt): Try[Relation] = ???
+  def process(delete: DeleteStmt): Try[Relation] = ???
+
 }
 
+/**
+ * An immutable, in-memory [[Relation]].
+ * @param rows
+ * @param attributes
+ */
 class View(
             val rows: Set[Row],
             val attributes: Seq[Attribute[_]]
-            ) extends Relation
+            ) extends Relation {
+  /**
+   * Add a new [[Row]] to this [[Relation]], returning a [[Try]] on a [[Relation]]with the row appended.
+   *
+   * In the case of mutable [[Relation]]s (i.e. [[deebee.storage.CSVRelation C S V R e l a t i o n]]), this
+   * can be a reference back to the same relation after it has been modified; while in the course of
+   * immutable relations (i.e. [[View]]) this can be a new relation with those rows appended. This
+   * is intended to ensure that the code for processing `INSERT` statements is polymorphic
+   * across mutable and immutable [[Relation]]s.
+   *
+   * If the relation cannot be modified, this returns a [[Try]] containing an
+   * [[deebee.exceptions.InternalStateException]].
+   *
+   * @param row the row to add
+   * @return a reference to a [[Relation]] with the row appended
+   */
+  override protected def add(row: deebee.Row): Try[Relation] = Success(
+    new View(rows + row, attributes)
+  )
+}

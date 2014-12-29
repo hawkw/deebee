097f4fffbfceac9e72d9b7b16e0f866fed617252
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-29 13:01:25 -0500

    Fix B+ tree stuff breaking the build

--- src/main/scala/treebee/BPlusTree.scala
+++ src/main/scala/treebee/BPlusTree.scala
@@ -5,19 +5,20 @@ import scala.collection.{SortedSetLike, SortedSet}
 /**
  * Created by hawk on 12/29/14.
  */
-trait BPlusTree[K,V] {
+trait BPlusTree[K <: Ordering[K],V] {
 
   protected val root: Branch[K]
 
   def find(key: K): V = find(key, root)
 
-  protected def find(key: K, node: Node): V = node match {
+  protected def find(key: K, node: Node[_]): V = node match {
     case Leaf(value: V) => value
+    case Branch(children) => ??? //todo: figure this out
   }
 
 }
 abstract class Node[A]
 
-case class Branch[A](children: Map[A,Node[_]], next: Option[Branch[A]]) extends Node[A]
+case class Branch[K <: Ordering[K]](children: Map[K,Node[K]]) extends Node[K]
 
 case class Leaf[A](contents: A) extends Node[A]
